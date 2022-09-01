cd42a28439a817c43ddf86c3f33a5d2126dd1056
weiyu.xiao <weiyu.xiao@outlook.com>
2022-09-01 22:50:46 +0800

    init

---
+++ src/NetCorePal.Yarp.ReverseProxy.ServiceDiscovery/ServiceDiscoveryProxyConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Primitives;
+using Yarp.ReverseProxy.Configuration;
+namespace NetCorePal.Yarp.ReverseProxy.ServiceDiscovery
+{
+    internal class ServiceDiscoveryProxyConfig : IProxyConfig
+    {
+
+        public List<RouteConfig> Routes { get; internal set; } = new List<RouteConfig>();
+
+        public List<ClusterConfig> Clusters { get; internal set; } = new List<ClusterConfig>();
+
+        IReadOnlyList<RouteConfig> IProxyConfig.Routes => Routes;
+
+        IReadOnlyList<ClusterConfig> IProxyConfig.Clusters => Clusters;
+
+        // This field is required.
+        public IChangeToken ChangeToken { get; internal set; } = default!;
+    }
+}
+
---
+++ src/NetCorePal.Yarp.ReverseProxy.ServiceDiscovery/ServiceDiscoveryProxyConfigProvider.cs
@@ -0,0 +1,28 @@
+﻿using NetCorePal.ServiceDiscovery;
+using Yarp.ReverseProxy.Configuration;
+
+namespace NetCorePal.Yarp.ReverseProxy.ServiceDiscovery
+{
+    public class ServiceDiscoveryProxyConfigProvider : IProxyConfigProvider
+    {
+        IServiceDiscoveryClient _client;
+        public ServiceDiscoveryProxyConfigProvider(IServiceDiscoveryClient serviceDiscoveryClient)
+        {
+            _client = serviceDiscoveryClient;
+        }
+
+        public IProxyConfig GetConfig()
+        {
+
+            var newservices = _client.GetServiceClusters().Select(p => new ClusterConfig
+            {
+
+                ClusterId = p.Value.ClusterId,
+                Metadata = p.Value.Metadata,
+                LoadBalancingPolicy = p.Value.LoadBalancingPolicy,
+                Destinations = p.Value.Destinations.ToDictionary(d => d.Key, d => new DestinationConfig { Address = d.Value.Address, Metadata = d.Value.Metadata })
+            });
+            return new ServiceDiscoveryProxyConfig { Clusters = newservices.ToList() };
+        }
+    }
+}
