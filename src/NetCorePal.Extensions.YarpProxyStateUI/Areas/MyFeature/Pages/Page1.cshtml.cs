3ce0af2e03a858cf1a888de9479aab41e8a068a9
witskeeper <witskeeper@gmail.com>
2022-10-17 15:47:15 +0800

    添加YarpProxyStateUI插件

---
+++ src/NetCorePal.Extensions.YarpProxyStateUI/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Yarp.ReverseProxy;
+namespace NetCorePal.Extensions.YarpProxyStateUI.MyFeature.Pages;
+
+public class Page1Model : PageModel
+{
+
+    public IProxyStateLookup ProxyStateLookup { get; init; }
+
+
+    public Page1Model(IProxyStateLookup proxyStateLookup)
+    {
+        this.ProxyStateLookup = proxyStateLookup;
+    }
+
+
+    public void OnGet()
+    {
+
+    }
+}
+
