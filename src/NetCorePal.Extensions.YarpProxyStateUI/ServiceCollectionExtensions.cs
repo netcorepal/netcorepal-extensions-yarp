2e08840f6031e0e3e2953024a79295f0211c2fbc
witskeeper <witskeeper@gmail.com>
2022-10-18 14:11:06 +0800

    添加bootstrap

---
+++ src/NetCorePal.Extensions.YarpProxyStateUI/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using NetCorePal.Extensions.YarpProxyStateUI;
+using Microsoft.Extensions.FileProviders;
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddYarpProxyStateUI(this IServiceCollection services)
+		{
+			services.AddRazorPages();
+			return services;
+		}
+		public static void UseYarpProxyStateUI(this IApplicationBuilder builder, int port)
+		{
+			builder.UseStaticFiles(new StaticFileOptions { FileProvider = new EmbeddedFileProvider(typeof(ServiceCollectionExtensions).Assembly) });
+			builder.UseEndpoints(endpoint => endpoint.MapRazorPages().RequireHost($"*:{port}"));
+		}
+	}
+}
+
