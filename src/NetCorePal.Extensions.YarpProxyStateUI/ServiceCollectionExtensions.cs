248d359e8407e92f93f94986ad737df44f860de6
witskeeper <witskeeper@gmail.com>
2022-11-21 22:44:22 +0800

    nullable port

--- src/NetCorePal.Extensions.YarpProxyStateUI/ServiceCollectionExtensions.cs
+++ src/NetCorePal.Extensions.YarpProxyStateUI/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using NetCorePal.Extensions.YarpProxyStateUI;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Builder;
 namespace Microsoft.Extensions.DependencyInjection
 {
 	public static class ServiceCollectionExtensions
@@ -13,15 +14,21 @@ namespace Microsoft.Extensions.DependencyInjection
 			services.AddRazorPages();
 			return services;
 		}
-		public static void UseYarpProxyStateUI(this IApplicationBuilder builder, int port)
+		public static void UseYarpProxyStateUI(this IApplicationBuilder builder, int? port = null)
 		{
 			builder.UseStaticFiles(new StaticFileOptions
 			{
 				RequestPath = "/_static",
 				FileProvider = new EmbeddedFileProvider(typeof(ServiceCollectionExtensions).Assembly, "NetCorePal.Extensions.YarpProxyStateUI.wwwroot")
 			});
-			builder.UseEndpoints(endpoint => endpoint.MapRazorPages().RequireHost($"*:{port}"));
-
+			if (port == null)
+			{
+				builder.UseEndpoints(endpoint => endpoint.MapRazorPages());
+			}
+			else
+			{
+				builder.UseEndpoints(endpoint => endpoint.MapRazorPages().RequireHost($"*:{port}"));
+			}
 		}
 	}
 }
